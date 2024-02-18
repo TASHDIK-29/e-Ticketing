05a4a34eabcd3048c629cf99e7eff087f1d253d7
Tashdik <s2110477153@ru.ac.bd>
2024-02-18 20:48:22 +0600

    main task completed

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -1,11 +1,58 @@
+let countSelect = 0;
+
 const seatContainer = document.getElementById('seat-container');
 
 const seatElements = seatContainer.querySelectorAll('button');
 
-for(const element of seatElements){
-    element.addEventListener('click', function(){
-        element.classList.add('bg-[#1DD100]');
-        countSelectedSeats(); 
-        
+for (const element of seatElements) {
+
+    element.addEventListener('click', function () {
+        countSelect++;
+
+        // Enable the Final Button
+        document.getElementById('final-btn').disabled = false ; 
+
+        if (countSelect <= 4) {
+            element.classList.add('bg-[#1DD100]');
+            countSelectedSeats();
+            addToCart(element.innerText);
+
+
+            element.disabled = true;
+        }
+        else {
+            alert('You can purchase maximum four tickets !');
+        }
+
     })
 }
+
+function applyCoupon(){
+    const element = document.getElementById('coupon-field');
+    const elementText = element.value ;
+
+    const actualElementText = elementText.split(' ').join('').toUpperCase();
+    
+    if(actualElementText === 'NEW15')
+    {
+        document.getElementById('grand-price').innerText = '1870' ;
+        element.classList.add('hidden');
+        document.getElementById('apply-btn').classList.add('hidden');
+    }
+
+    else if(actualElementText === 'COUPLE20')
+    {
+        document.getElementById('grand-price').innerText = '1760' ;
+        element.classList.add('hidden');
+        document.getElementById('apply-btn').classList.add('hidden');
+
+    }
+
+    else{
+        alert('Invalid Coupon !');
+    }
+    
+
+}
+
+
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,17 +1,67 @@
 let seatCount = 0;
-function countSelectedSeats(){
-    seatCount ++ ;
+
+function countSelectedSeats() {
+    seatCount++;
     const element = document.getElementById('selected-seat');
-    element.innerText = seatCount ;
-    console.log(element.innerText);
+    element.innerText = seatCount;
+
+    // coupon Button
+    const element2 = document.getElementById('apply-btn');
+    
+    if(seatCount === 4)
+    {
+        element2.disabled = false ;
+        element2.classList.remove('bg-slate-400');
+        element2.classList.add('bg-[#1DD100]');
+    }
+
     remainingSeats();
+    
 }
 
-function remainingSeats(){
+function remainingSeats() {
     const element = document.getElementById('remaining-seat');
 
     const booked = parseInt(seatCount);
     const available = parseInt(40);
+
+    element.innerText = available - booked;
+    setTotalPrice(booked);
+    setGrandPrice(booked);
+}
+
+function addToCart(elementText) {
+    const element = document.getElementById('add-to-cart');
+
+    // Create child div
+    const newDiv = document.createElement('div');
+    newDiv.innerHTML = `
+    <h1>elementText</h1>
+    <h2>Economy</h2>
+    <h3>550</h3>
+
+    `
+    //newDiv.classList.add('flex justify-between');
+    element.append(newDiv);
+    newDiv.classList.add('flex');
+    newDiv.classList.add('justify-between');
+
     
-    element.innerText = available - booked ;
+    newDiv.querySelector('h1').innerText = elementText ;
+    newDiv.querySelector('h1').classList.add('inter');
+    newDiv.querySelector('h2').classList.add('inter');
+    newDiv.querySelector('h3').classList.add('inter');
+    
+}
+
+function setTotalPrice(booked){
+    const element = document.getElementById('total-price');
+    element.classList.add('inter');
+    element.innerText = booked * 550 ;
+}
+
+function setGrandPrice(booked){
+    const element = document.getElementById('grand-price');
+    element.classList.add('inter');
+    element.innerText = booked * 550 ;
 }
